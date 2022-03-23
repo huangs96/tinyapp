0f9d0f35ccb7a1cce55c6a664f9119aa16127be7
Stephen Huang <stephen_1996@live.ca>
2022-03-23 09:34:21 +0000

    added usernames

--- express_server.js
+++ express_server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require('cookie-parser')
 
 app.set("view engine", "ejs");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 function generateRandomString() {
   let chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
@@ -25,45 +30,67 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const urlData = { username: req.cookies["username"]};
+  res.render("urls_new", urlData);
 });
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  let randomURL = generateRandomString();
-  urlDatabase[randomURL] = req.body.longURL;
-  res.redirect(`/urls/${randomURL}`);         // Respond with 'Ok' (we will replace this)
-});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
 app.get("/urls", (req, res) => {
-  const urlData = { urls: urlDatabase };
+  const urlData = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", urlData);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL] //req.params.shortURL grabs the key of whatever I input into the /urls/:shortURL
-  const urlData2 = { shortURL: req.params.shortURL, longURL: longURL } //shortURL equals to req.params.shortURL (the shortURL here is replaced by whatever is put into the shortURL in line)
+  const urlData2 = { shortURL: req.params.shortURL, longURL: longURL, username: req.cookies["username"] } //shortURL equals to req.params.shortURL (the shortURL here is replaced by whatever is put into the shortURL in line)
   res.render("urls_show", urlData2);
+});
+
+app.post("/login", (req, res) => {
+  let userID = req.body.username;
+  res.cookie("username", userID);
+  res.redirect("/urls")
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
-  const longURL = urlDatabase[shortURL];
   delete urlDatabase[shortURL]
   res.redirect("/urls");
 })
+
+app.post("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  urlDatabase[id] = req.body.id;
+  res.render("/urls/");
+})
+
+app.post("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const userData = {shortURL: shortURL};
+  res.redirect(`/urls/${shortURL}`);
+});
 
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL]
   res.redirect(longURL);
+});
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  let randomURL = generateRandomString();
+  urlDatabase[randomURL] = req.body.longURL;
+  res.redirect(`/urls/${randomURL}`);         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/hello", (req, res) => {
